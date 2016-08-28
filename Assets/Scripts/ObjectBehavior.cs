b8278f5b1aa63bf496766433e1f3952392642203
Simon Vinberg Andersen <mr.simon3500@gmail.com>
2016-08-28 18:44:27 +0200

    Added a drawray on player, set up the basic function for throwing things around.

---
+++ Assets/Scripts/ObjectBehavior.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectBehavior : MonoBehaviour
+{
+	public bool IsPickedUp;
+	public Vector3 TargetPosition;
+	public float DistanceFromCamera;
+
+	private Vector3 targetDirection;
+
+	// Use this for initialization
+	void Start ()
+	{
+		IsPickedUp = false;
+		TargetPosition = Vector3.zero;
+		targetDirection = Vector3.zero;
+		DistanceFromCamera = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (IsPickedUp)
+		{
+			LerpFunction(TargetPosition);
+		}
+		else
+		{
+			GetComponent<Rigidbody>().useGravity = true;
+			//GetComponent<Rigidbody>().angularDrag = 0.05f;
+		}
+	}
+
+	void LerpFunction(Vector3 trgtpos)
+	{
+		GetComponent<Rigidbody>().useGravity = false;
+		GetComponent<Rigidbody>().angularDrag = 0.5f;
+		targetDirection = trgtpos - transform.position;
+		Debug.Log(targetDirection);
+
+		if (Mathf.Sqrt(targetDirection.sqrMagnitude) > 0.01f)
+		{
+			GetComponent<Rigidbody>().velocity = targetDirection;
+		}
+
+		//GetComponent<Rigidbody>().AddForce(targetDirection,ForceMode.Force);
+	}
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.DrawRay(transform.position, transform.forward*3, Color.red);
+	}
+}
