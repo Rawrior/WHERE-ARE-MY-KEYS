8ec36e0737b9bd1aef53e077b5ceda559de59b8b
Simon Vinberg Andersen <mr.simon3500@gmail.com>
2016-09-11 16:40:25 +0200

    Object change color properly! Yellow on, yellow off. Next-up is pick-up.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,6 +6,8 @@ public class PlayerBehavior : MonoBehaviour
 	public LayerMask Mask;
 	public GameObject LastHit;
 
+	private bool isHoldingObject;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -17,25 +19,54 @@ public class PlayerBehavior : MonoBehaviour
 	{
 		Debug.DrawRay(transform.position, transform.forward*3, Color.red);
 
-		RaycastMethod(Mask);
+		if (!isHoldingObject)
+			RaycastMethod(Mask);
 
 	}
 
-	void RaycastMethod(LayerMask layermask)
+	bool RaycastMethod(LayerMask layermask)
 	{
+		//Declare variable for the out-value of z
 		RaycastHit RayHit;
-		if (Physics.Raycast(transform.position, transform.forward * 3, out RayHit, 3f, layermask))
-		{
-			//Physics.Raycast(transform.position, Vector3.forward*3, out RayHit, 100f, layermask);
 
-			//TODO: Fix this shit. It's returning a nullreferenceexception from the editor. Something about the RayHit not being assigned? Research it and find out.
-			Debug.Log(RayHit.collider.name);
+		Physics.Raycast(transform.position, transform.forward*3, out RayHit, 3f, layermask);
+
+		if (RayHit.collider != null)
+		{
+			if (RayHit.collider.gameObject != LastHit && LastHit != null)
+			{
+				LastHit.GetComponent<MeshRenderer>().material = LastHit.GetComponent<ObjectBehavior>().RestMaterial;
+				//Changing to rest material	
+				Debug.Log("Changing material to rest material");
+				LastHit = RayHit.collider.gameObject;
+			}
 
 			if (RayHit.transform.tag == "Pickupable")
 			{
+				//Changing material to Hover material
+				Debug.Log("Changing material to Hover material");
 				LastHit = RayHit.collider.gameObject;
 				RayHit.collider.GetComponent<MeshRenderer>().material = RayHit.collider.GetComponent<ObjectBehavior>().HoverMaterial;
 			}
+
+			Debug.Log("Returning true");
+			return true;
 		}
+
+		if (RayHit.collider == null/* && LastHit != null*/)
+		{
+			//Changing material to Rest material
+			Debug.Log("Changing material to rest material");
+			LastHit.GetComponent<MeshRenderer>().material = LastHit.GetComponent<ObjectBehavior>().RestMaterial;
+			LastHit = null;
+			Debug.Log("Returning false");
+			return false;
+		}
+
+		//if (RayHit.collider == null)
+		//{
+			
+		//}
+		return false;
 	}
 }
